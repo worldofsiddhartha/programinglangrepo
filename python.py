3ca55387189094d3d5827283ca063cfe2f9403c9
worldofsiddhartha <Lakkakulasiddhartha9@gmail.com>
2024-11-21 22:32:24 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,4 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("siddhartha")
+siddhartha
